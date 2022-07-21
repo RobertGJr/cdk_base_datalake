50a433990191f888fd59642ac9149f8e16f5e78f
Roberto Gonzalez <rgonzalez@macbook-2017-1.local>
2022-07-20 21:14:47 -0400

    first push for foundational datalake

---
+++ cdk_base_datalake/test/cdk_base_datalake.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBaseDatalake from '../lib/cdk_base_datalake-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_base_datalake-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBaseDatalake.CdkBaseDatalakeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
